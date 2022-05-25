95d411f822282ace86be7dbf88ad6015c9d964c2
Christian Lessa <christianclessa@gmail.com>
2022-05-25 12:26:12 -0300

    Requisito 7 concluido - Criando model Category

---
+++ src/database/models/category.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const Categories = sequelize.define("Category", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING
+  }, {
+    timestamps: false,
+  });
+
+  return Categories;
+};
